f6e7580f71fb883e3b72c87e40b584b3d0516473
edinsonsuno-rgb <TU_EMAIL_DE_GITHUB>
2026-07-22 22:38:09 -0500

    color del fondo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ export default {
         display: ['Bebas Neue', 'sans-serif'],
       },
       colors: {
-        'df-bg': '#121212',
+        'df-bg': '#4B4B4F',
         'df-card': '#161B22',
         'df-surface': '#21262D',
         'df-border': '#30363D',
